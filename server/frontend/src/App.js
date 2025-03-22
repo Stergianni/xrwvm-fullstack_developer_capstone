16c48fa610badb267e301451d65504513927f977
stergianni <stella.orfanidis@hotmail.com>
2025-03-22 11:29:49 -0400

    User Management

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import React from "react";
+import Register from "./components/Register/Register"
 
 function App() {
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
