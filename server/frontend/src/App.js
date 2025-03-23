bc2c18b0efdd9e81ae43f27d455e33fc8b95ebb6
Stergianni <stella.orfanidis@hotmail.com>
2025-03-23 18:10:04 +0100

    /*jshint esversion: 8 */

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 8 */
+
 import PostReview from "./components/Dealers/PostReview"
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
