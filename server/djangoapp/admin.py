6952322e50665882ce4e0aa176d436b0a5ad6de6
Stergianni <stella.orfanidis@hotmail.com>
2025-03-24 19:25:37 +0100

    commit test

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,14 +2,22 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 
-# Registering models with their respective admins
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
-# CarModelInline class
 
-# CarModelAdmin class
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    inlines = [CarModelInline]
 
-# CarMakeAdmin class with CarModelInline
 
-# Register models here
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+    list_filter = ('car_make', 'type', 'year')
+    search_fields = ('name', 'car_make__name')
+
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,35 +1,43 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create a Car Make model
+
 class CarMake(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    
+    """
+    Modelo que representa una marca de automóvil.
+    """
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
     def __str__(self):
         return self.name
 
-# Create a Car Model model
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    """
+    Modelo que representa un modelo de automóvil asociado a una marca.
+    """
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )  # Relación muchos a uno
     name = models.CharField(max_length=100)
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        ('TRUCK', 'Truck'),
-        ('COUPE', 'Coupe')
+        ('HATCHBACK', 'Hatchback'),
+        ('CONVERTIBLE', 'Convertible'),
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+
+    type = models.CharField(max_length=15, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(
-        default=2023,
         validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
+            MinValueValidator(2015),  # Año mínimo permitido
+            MaxValueValidator(2025),  # Año máximo permitido
         ]
     )
-    dealer_id = models.IntegerField()  # Refers to a dealer in Cloudant database
-    
+    dealer_id = models.PositiveIntegerField()  # ID del concesionario
+
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.year})"
